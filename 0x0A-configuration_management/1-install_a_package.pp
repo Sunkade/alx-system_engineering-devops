ae9a500827420528be6efcf2f74265da1995a099
Sunkade <sunkadefasola@gmail.com>
2023-11-25 01:05:38 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,7 @@
 #!/usr/bin/env puppet
 # Using Puppet to create a manifest that kills a process named killmenow.
-
-exec { 'kill':
-  command   => 'pkill -f killmenow',
-  path      => ['/usr/bin', '/usr/sbin'],
-  returns   => ['0', '1'],
-  logoutput => true,
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
