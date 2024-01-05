c08ae40a523c8791ab18d4380b8eaf62f7b1cd9d
Sunkade <sunkadefasola@gmail.com>
2024-01-05 14:42:23 +0100

    Make check_mysql_installed.py executable

--- 0x14-mysql/0-install_mysql.sh
+++ 0x14-mysql/0-install_mysql.sh
@@ -3,7 +3,16 @@
 # Add MySQL APT repository
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.16-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.16-1_all.deb
-echo "mysql-apt-config mysql-apt-config/select-server select mysql-5.7" | sudo debconf-set-selections
+
+# Set non-interactive mode for debconf
+export DEBIAN_FRONTEND=noninteractive
+
+# Automatically configure MySQL APT repository without prompts
+sudo debconf-set-selections <<< "mysql-apt-config mysql-apt-config/select-server select mysql-5.7"
+sudo debconf-set-selections <<< "mysql-apt-config mysql-apt-config/select-tools select Enabled"
+sudo debconf-set-selections <<< "mysql-apt-config mysql-apt-config/select-preview select Disabled"
+
+# Update package list
 sudo apt-get update
 
 # Install MySQL 5.7
