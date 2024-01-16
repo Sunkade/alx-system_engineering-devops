4efbb70c1713ec98bc76391e74b4a768016351a4
Sunkade <sunkadefasola@gmail.com>
2024-01-17 00:35:03 +0100

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,50 +1,36 @@
 #!/usr/bin/python3
-""" Module for a function that queries the Reddit API recursively."""
-
-
+'''
+    This module contains the function recurse
+'''
 import requests
+from sys import argv
 
-
-def count_words(subreddit, word_list, after='', word_dict={}):
-    """ A function that queries the Reddit API parses the title of
-    all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces.
-    Javascript should count as javascript, but java should not).
-    If no posts match or the subreddit is invalid, it prints nothing.
-    """
-
-    if not word_dict:
-        for word in word_list:
-            if word.lower() not in word_dict:
-                word_dict[word.lower()] = 0
-
-    if after is None:
-        wordict = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
-        for word in wordict:
-            if word[1]:
-                print('{}: {}'.format(word[0], word[1]))
-        return None
-
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    header = {'user-agent': 'redquery'}
-    parameters = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=header, params=parameters,
-                            allow_redirects=False)
-
-    if response.status_code != 200:
-        return None
-
+def recurse(subreddit, hot_list=[], after=None):
+    '''
+        Recursively queries the Reddit API and returns a list
+        containing the titles of all hot articles for a given subreddit.
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    params = {'limit': 100, 'after': after}
+    url = requests.get(f'https://www.reddit.com/r/{subreddit}/hot/.json', headers=user, params=params)
+    
     try:
-        hot = response.json()['data']['children']
-        aft = response.json()['data']['after']
-        for post in hot:
-            title = post['data']['title']
-            lower = [word.lower() for word in title.split(' ')]
-
-            for word in word_dict.keys():
-                word_dict[word] += lower.count(word)
-
+        data = url.json().get('data')
+        after = data.get('after')
+        children = data.get('children')
+        
+        if after is not None and children:
+            for post in children:
+                hot_list.append(post.get('data').get('title'))
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
     except Exception:
         return None
 
-    count_words(subreddit, word_list, aft, word_dict)
+if __name__ == "__main__":
+    result = recurse(argv[1])
+    if result is not None:
+        print(len(result))
+    else:
+        print("None")
