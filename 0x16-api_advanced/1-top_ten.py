cd528947bc4868a0f34b7e798700ccaf0d60ed7f
Sunkade <sunkadefasola@gmail.com>
2024-01-17 00:28:22 +0100

    0-subs.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""
-1-top_ten
-"""
+'''
+    This module contains the function top_ten
+'''
 import requests
 from sys import argv
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
-    """
+    '''
+        Returns the top ten posts for a given subreddit
+    '''
     user_agent = {'User-Agent': 'Lizzie'}
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
     response = requests.get(url, headers=user_agent)
 
     if response.status_code == 200:
-        data = response.json().get('data', {}).get('children', [])
-        for post in data:
-            print(post['data']['title'])
+        try:
+            for post in response.json().get('data', {}).get('children', []):
+                print(post.get('data', {}).get('title'))
+        except KeyError:
+            print("Error: Unexpected JSON structure.")
     else:
         print(None)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
