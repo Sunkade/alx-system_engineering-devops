f18cf96bca583bb2a0e4f6c82a6ba2f9ac0ed54e
Sunkade <sunkadefasola@gmail.com>
2024-01-17 00:38:10 +0100

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,32 +1,54 @@
 #!/usr/bin/python3
-"""
-100-count
-"""
+'''
+    This module contains the function count_words
+'''
 import requests
+from sys import argv
 
-def count_words(subreddit, word_list, hot_list=None):
-    """
-    Recursively queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords.
+def count_words(subreddit, word_list, hot_list=[], after=None):
+    '''
+        Recursively queries the Reddit API, parses the title of all hot articles,
+        and prints a sorted count of given keywords.
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    params = {'limit': 100, 'after': after}
+    url = requests.get(f'https://www.reddit.com/r/{subreddit}/hot/.json', headers=user, params=params)
+    
+    try:
+        data = url.json().get('data')
+        after = data.get('after')
+        children = data.get('children')
+        
+        if after is not None and children:
+            for post in children:
+                hot_list.append(post.get('data').get('title'))
+            
+            return count_words(subreddit, word_list, hot_list, after)
+        else:
+            print_word_count(word_list, hot_list)
+    except Exception:
+        print(None)
 
-    :param subreddit: The subreddit to search.
-    :param word_list: List of keywords.
-    :param hot_list: List containing titles of hot articles. If not provided, it will be fetched.
-    """
-    if hot_list is None:
-        hot_list = recurse(subreddit)
-
-    if hot_list is None:
-        return
-
+def print_word_count(word_list, hot_list):
+    '''
+        Prints the sorted count of given keywords.
+    '''
     word_count = {}
     for word in word_list:
         word_count[word.lower()] = 0
-
+    
     for title in hot_list:
-        words = title.lower().split()
         for word in word_list:
-            word_count[word.lower()] += words.count(word.lower())
+            word_count[word.lower()] += title.lower().split().count(word.lower())
+    
+    sorted_word_count = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+    
+    for word, count in sorted_word_count:
+        print(f"{word}: {count}")
 
-    sorted_count = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
-
-    for word, count in sorted_count
+if __name__ == "__main__":
+    if len(argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(argv[0]))
+    else:
+        count_words(argv[1], [x for x in argv[2].split()])
