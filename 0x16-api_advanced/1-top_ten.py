80dee4596c6aae36c10f2844924f539a4de734c1
Sunkade <sunkadefasola@gmail.com>
2024-01-17 00:26:02 +0100

    0-subs.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-'''
-    this module contains the function number_of_subscribers
-'''
+"""
+1-top_ten
+"""
 import requests
 from sys import argv
 
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    """
+    user_agent = {'User-Agent': 'Lizzie'}
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    response = requests.get(url, headers=user_agent)
 
-def number_of_subscribers(subreddit):
-    '''
-        returns the number of subscribers for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
-        return 0
+    if response.status_code == 200:
+        data = response.json().get('data', {}).get('children', [])
+        for post in data:
+            print(post['data']['title'])
+    else:
+        print(None)
 
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+if __name__ == '__main__':
+    if len(argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(argv[1])
