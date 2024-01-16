b263315a125cc1e5251d9166c9fe5e0d953a2930
Sunkade <sunkadefasola@gmail.com>
2024-01-17 00:15:30 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,17 +8,18 @@ def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
 
-    :param subreddit: The subreddit to search.
-    :return: Number of subscribers or 0 if invalid subreddit.
+    :param subreddit: The subreddit to query.
+    :return: Number of subscribers or 0 if not a valid subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
+        subscribers_count = data['data']['subscribers']
+        return subscribers_count
     else:
         return 0
 
@@ -28,4 +29,5 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        result = number_of_subscribers(sys.argv[1])
+        print(result)
